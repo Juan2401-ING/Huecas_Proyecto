66e6523bf273e2fa9b02064bd22fd04a3af99ce0
jpbenalcazar <jpbenalcazar@puce.edu.ec>
2022-10-16 11:17:54 -0500

    Traer imagen desde la base de datos JPB.

--- app/src/main/java/com/example/huecas002/HuecaAdapter.java
+++ app/src/main/java/com/example/huecas002/HuecaAdapter.java
@@ -3,13 +3,18 @@ package com.example.huecas002;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 public class HuecaAdapter extends FirestoreRecyclerAdapter <Hueca, HuecaAdapter.HuecaHolder> {
 
@@ -18,10 +23,22 @@ public class HuecaAdapter extends FirestoreRecyclerAdapter <Hueca, HuecaAdapter.
     }
 
     @Override
-    //Ejemplo2github
     protected void onBindViewHolder(@NonNull HuecaHolder holder, int position, @NonNull Hueca model) {
      holder.textViewnombre.setText(model.getNombre());
      holder.textViewprioridad.setText(String.valueOf(model.getPrioridad()));
+     Picasso.get().load(model.getImagen()).into(holder.imagenhueca, new Callback(){
+
+         @Override
+         public void onSuccess() {
+
+         }
+
+         @Override
+         public void onError(Exception e) {
+             //Toast.makeText(getItem(), "No se ha podido cargar la imagen", Toast.LENGTH_LONG).show();
+
+         }
+     });
     }
 
     @NonNull
@@ -32,13 +49,16 @@ public class HuecaAdapter extends FirestoreRecyclerAdapter <Hueca, HuecaAdapter.
     }
 
     class HuecaHolder extends RecyclerView.ViewHolder {
+        //Atributos de la base de datos
         TextView textViewnombre;
         TextView textViewprioridad;
+        ImageView imagenhueca;
 
         public HuecaHolder(View itemView){
             super(itemView);
             textViewnombre = itemView.findViewById(R.id.text_huecanombre);
             textViewprioridad = itemView.findViewById(R.id.text_priority);
+            imagenhueca=itemView.findViewById(R.id.imagenView);
         }
     }
 }
